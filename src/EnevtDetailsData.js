06d2edd158a3ebf880ea2004ce9851913226deec
anuragkj <f20200128@dubai.bits-pilani.ac.in>
2023-09-24 14:47:30 +0400

    Test commit

--- src/EnevtDetailsData.js
+++ src/EnevtDetailsData.js
@@ -30,4 +30,34 @@ export const events = {
     image: "https://a.l3n.co/i/d1J2gM.png",
     registrationLink: null,
   },
+  "info-session-2": {
+    name: "GDSC Essentials",
+    date: "20th September 2023",
+    dateTime: "2023-09-20 10:00:00",
+    shortDesc:
+      "This session is going to be an introduction to the new GDSC BPDC.",
+    postEventDesc: "",
+    url: "info-session-2",
+    description: (
+      <p>
+        We have observed that a lot of people are curious about GDSC and how it
+        works Shrouded by the mist of dubiety, we are organizing an Informative
+        Session on 15th September 8 p.m., that will clear all your doubts
+        <br />
+        <br />
+        The Informative Session will include discussions on:-
+        <ul>
+          <li>Our Mission</li>
+          <li>Our Goals</li>
+          <li>Our Expectations</li>
+          <li>Our way of Operation</li>
+          <li>And, much more!</li>
+        </ul>
+        Make sure to attend the session and become a part of our growing
+        community
+      </p>
+    ),
+    image: "https://a.l3n.co/i/d1J2gM.png",
+    registrationLink: null,
+  },
 };
