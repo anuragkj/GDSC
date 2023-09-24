27f3cc6911c6a8e4929634977c0461346ab55059
Riddhi Goswami <91322531+Secret-Ambush@users.noreply.github.com>
2023-09-24 17:50:44 +0000

    added Campus Quest event details

--- src/EnevtDetailsData.js
+++ src/EnevtDetailsData.js
@@ -1,5 +1,24 @@
 import React from "react";
 export const events = {
+  "treasure-hunt-session": {
+    name: "Campus Quest",
+    date: "5th September 2023",
+    dateTime: "2023-09-05 09:00:00",
+    shortDesc:
+      "This session is going to be an introduction to the new GDSC BPDC.",
+    postEventDesc: "",
+    url: "info-session",
+    description: (
+      <p>
+        A scavenger hunt that takes you all around campus! Solve riddles, 
+        scan the corresponding QR codes located all over the campus 
+        but make sure you do it as fast as possible!
+        <br />
+      </p>
+    ),
+    image: "https://c.l3n.co/i/d4PVr7.jpeg",
+    registrationLink: null,
+  },
   "info-session": {
     name: "GDSC Essentials",
     date: "15th September 2023",
